557230b8957128f58575bf06d67a60b4156f07b7
PiqqiDesigns <eric.otuvaka@gmail.com>
2022-09-30 15:39:47 -0700

    feat: implement working API routing from index page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-npm run dev to start local development server (localhost:3000)
-npm run storybook to start storybook server for component editing (localhost:6006)
+"npm run dev" to start local development server (localhost:3000)
+"npm run storybook" to start storybook server for component editing (localhost:6006)
+
+BEFORE "git push -u", NEED TO CLOSE ALL LOCALHOST SERVERS
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
 import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import Search from '../components/utility/search/Search';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
-  const { locale } = useRouter();
-
   return (
     <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
       <Image
@@ -19,12 +15,7 @@ const Home: NextPageWithLayout = () => {
         priority
       />
       <Search />
-      <p>
-        Google offered in:{' '}
-        <Link href="/" locale={locale === 'en' ? 'fr' : 'en'}>
-          <a className="underline text-blue-600">Francais</a>
-        </Link>
-      </p>
+      <p>Search for dogs, cats, or broccoli!</p>
     </section>
   );
 };
